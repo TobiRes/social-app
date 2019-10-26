4746abc1c12ce62f32e22fabfc181ba693d6b49f
Tobias Reski <tobias.reski@gmail.com>
2019-10-26 13:46:37 +0200

    Landing page styling

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  menuActive = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onBurgerClicked() {
+    this.menuActive = !this.menuActive;
+  }
 }
