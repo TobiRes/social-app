55b8165f4d2a98d8fb1bee3655be0a80d34dc4a9
Tobias Reski <tobias.reski@gmail.com>
2019-10-26 11:47:45 +0200

    WIP: Masked wave image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,36 @@ import { MainContainerComponent } from '../components/main-container/main-contai
 import {StoreModule} from '@ngrx/store';
 import {reducers} from '../store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {RouterModule, Routes} from '@angular/router';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { LandingPageComponent } from '../components/landing-page/landing-page.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'welcome',
+    component: LandingPageComponent
+  },
+  { path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainContainerComponent
+    MainContainerComponent,
+    NavbarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument(),
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
